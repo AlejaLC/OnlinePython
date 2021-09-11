8c4a6227b550559096e5fc504ff82be4eab74fab
AlejaLC <89111814+AlejaLC@users.noreply.github.com>
2021-09-10 20:36:16 -0500

    Add files via upload

---
+++ AlejandraLopez_Exercise3.py
@@ -0,0 +1,21 @@
+# Bad Variable Names
+
+
+x = 7          # one random letter assignment to a value not knowing what it represents
+
+areaCm = 7     # harder to read
+
+elif = 7       # variable name can't be a keyword, Syntax error shown below
+
+# Good Variable Names
+
+
+hours = 7      # naming the actual name of what the value represents
+
+area_cm = 7    # using underscore to separate variable name makes it easier to read
+
+area_M2 - 7    # Mnemonic variable names to help remember why variable was created
+
+
+
+
